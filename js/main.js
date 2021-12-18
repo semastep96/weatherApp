d6795b63ed4f8e6f908bf101ce203f20e7008707
semastep96 <semastep5@gmail.com>
2021-12-18 19:15:58 +0300

    change http on https

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 import {UI} from './view.js'
-const serverUrl = 'http://api.openweathermap.org/data/2.5/weather';
+const serverUrl = 'https://api.openweathermap.org/data/2.5/weather';
 const apiKey = 'f660a2fb1e4bad108d6160b7f58c555f';
 let newInfo = null;
 const favoriteLocations = []
